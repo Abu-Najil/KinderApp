32856058048330abca7b0c7f8f8befd9683c86fb
Aldin Kolic <110829508+Abu-Najil@users.noreply.github.com>
2024-07-14 12:26:26 +0200

    Initial Commit

---
+++ KinderApp/KinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinderAppApp.swift
+//  KinderApp
+//
+//  Created by Aldin Kolic on 14.07.24.
+//
+
+import SwiftUI
+
+@main
+struct KinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
